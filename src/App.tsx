bac03fcfea067ba4f654cf6b043b762e7632ad6a
devmuraCode <muradkusherbaev@gmail.com>
2024-03-21 17:10:40 +0500

    adding component Reviews

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
+import Reviews from "./components/reviews/Reviews";
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <Reviews/>
       <About/>
+      
     </div>
   );
 };
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import cls from './Footer.module.scss'
+const Footer = () => {
+  return (
+    <div className={cls.wrapper}>
+
+    </div>
+  )
+}
+
+export default Footer
