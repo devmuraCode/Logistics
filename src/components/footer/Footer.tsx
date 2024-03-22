7eae14faac7b29518c449b003a1013ba18f4f9ab
KamilCodeDev <akadel.76@gmail.com>
2024-03-22 20:13:23 +0500

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,11 @@
 import cls from './Footer.module.scss'
+import { ArrowUpOutlined } from '@ant-design/icons'
 const Footer = () => {
   return (
     <div className={cls.wrapper}>
-
+      <ArrowUpOutlined style={{ color: 'white', fontSize: '50px' }} />
+      <a href='#' className=' text-lg text-white'>Наверх</a>
+      <p >©2035 Володин и Морозова. Сайт создан на Wix.com</p>
     </div>
   )
 }
