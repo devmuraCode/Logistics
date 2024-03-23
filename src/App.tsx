c57f4f953521e524ca9b929ad0555ea4f99b99a2
devmuraCode <muradkusherbaev@gmail.com>
2024-03-24 03:13:08 +0500

    adding component delivery

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Spheres from "./components/spheres/Spheres";
 import Basic from "./components/basic/Basic";
+import Delivery from "./components/delivery/Delivery";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
       <Basic/>
-      <Spheres/>
+      <Delivery/>
       <Footer />
     </div>
   );
