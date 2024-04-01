77c07685ea000804c0f952017a45166841b04540
KamilCodeDev <akadel.76@gmail.com>
2024-04-01 20:00:41 +0500

    hello world

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,7 +1,33 @@
-
+import cls from "./Section.module.scss"
+import img3 from "../../assets/img3.jpeg"
 const Section = () => {
   return (
-    <div></div>
+    <div className="max-w-6xl mx-auto ">
+      <h1 className="text-3xl font-bold text-center mb-9">КАК ЗАРАБАТЫВАТЬ НА ПАРТНЕРСКОЙ ПРОГРАМЕ </h1>
+      <div className={cls.Card}>
+        <div className={cls.Cards}>
+          <img className="w-14 h-14" src={img3} alt="" />
+          <div className="ml-9">
+            <h3>Регистрацтя на сайте </h3>
+            <p>Регистрируйте в нашей портнерской програме для вебмастеров или активизируйте ее если уже являетесь нашим клиентом </p>
+          </div>
+        </div>
+        <div className={cls.Cards}>
+          <img className="w-14 h-14" src={img3} alt="" />
+          <div className="ml-9">
+            <h3>Регистрацтя на сайте </h3>
+            <p>Регистрируйте в нашей портнерской програме для вебмастеров или активизируйте ее если уже являетесь нашим клиентом </p>
+          </div>
+        </div>
+        <div className={cls.Cards}>
+          <img className="w-14 h-14" src={img3} alt="" />
+          <div className="ml-9">
+            <h3>Регистрацтя на сайте </h3>
+            <p>Регистрируйте в нашей портнерской програме для вебмастеров или активизируйте ее если уже являетесь нашим клиентом </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
