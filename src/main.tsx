bdcc000a9f3c033ccb2050710a16452dfad34ae4
devmuraCode <muradkusherbaev@gmail.com>
2024-10-27 23:20:47 +0500

    sdf

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import './index.css'
-import App from './App.tsx'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <QueryClientProvider client={queryClient}><App /></QueryClientProvider>
-  </HashRouter>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+
+      <App />
+
+  </QueryClientProvider>
+);
