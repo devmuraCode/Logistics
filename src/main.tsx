ed4e8e8df364719efd633e3bdbfdb9bce13ada0d
devmuraCode <muradkusherbaev@gmail.com>
2024-10-27 22:59:18 +0500

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <>
+  <HashRouter>
     <QueryClientProvider client={queryClient}><App /></QueryClientProvider>
-  </>,
+  </HashRouter>,
 )
